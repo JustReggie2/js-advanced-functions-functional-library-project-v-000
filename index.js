3c15e621edadc83ec7beea46a38d646b7cdc39c4
github username <github email address>
2020-01-27 22:33:51 -0500

    Done.

--- index.js
+++ index.js
@@ -4,20 +4,65 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      if (typeof collection != "undefined") {
+        const x = Object.values(collection);
+        for (var i = 0; i < x.length; i++) {
+          callback(x[i])
+        }
+      } else {
+        for (var i = 0; i < collection.length; i++) {
+          callback(collection[i])
+        }
+      }
+      return collection
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+      const x1 = []
+      if (typeof collection != "undefined") {
+        const x = Object.values(collection);
+        for (var i = 0; i < x.length; i++) {
+          x1.push(callback(x[i]))
+        }
+      } else {
+        for (var i = 0; i < collection.length; i++) {
+          x1.push(callback(collection[i]))
+        }
+      }
+      return x1
     },
 
-    reduce: function() {
-
+    reduce: function(collection, callback, acc) {
+      let total = 0;
+      for (var i = 0; i < collection.length; i++) {
+        total = callback(total, collection[i], collection)
+      }
+      if (!acc) {
+        return total
+      } else {
+        return total + acc
+      }
     },
 
-    functions: function() {
+    find: function(collection, predicate) {
+      for (var i = 0; i < collection.length; i++) {
+        let x = predicate(collection[i])
+        if (x === collection[i]) {
+          return collection[i]
+        } 
+      }
+    },
 
+    functions: function(object) {
+      const x = []
+      for (const [k, v] of Object.entries(object)) {
+        if (typeof v === "function") {
+          x.push(k)
+        }
+      }
+      x.sort()
+      return x
     },
 
 
